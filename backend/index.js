c59ad78f75917e9efef17275fdb2d808e95e0992
Mario <daniellopezbosque@gmail.com>
2022-10-11 21:07:10 -0600

    Arreglo errores generales

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 
 //Una por ruta OJO
-app.use(require('./rutas/'));
+//app.use(require('./routes'));
 
-app.listes(app.get('port'), () => {
-    console.log('Port: ', app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('Port:', app.get('port'));
 });
