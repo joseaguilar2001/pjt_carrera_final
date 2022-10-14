90fd522c92ba1c20ed9a261630dc2b5f693c053f
SofiaGalicia <sofiagalicia001@mesoamericana.edu.gt>
2022-10-13 19:49:22 -0600

    detalleKardex y lotes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ var solicitanteRoute = require('./routes/solicitante');
 var ejecutorRoute = require('./routes/ejecutores');
 var kardexRoute = require('./routes/kardex');
 var productoRoute = require('./routes/producto');
+var loteRoute = require('./routes/lotes');
+var dKardexRoute = require('./routes/detalleKardex');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
@@ -17,6 +19,8 @@ app.use('/solicitante', solicitanteRoute);
 app.use('/ejecutores', ejecutorRoute);
 app.use('/kardex', kardexRoute);
 app.use('/producto', productoRoute);
+app.use('/lotes', loteRoute);
+app.use('/detalleKardex', dKardexRoute);
 
 app.listen(app.get('port'), () => {
     console.log('Port:', app.get('port'));
