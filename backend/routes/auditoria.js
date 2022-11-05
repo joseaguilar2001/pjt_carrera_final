565a31b56ab536149b078a29b3f51b2f708af5e9
Mario <daniellopezbosque@gmail.com>
2022-11-05 00:27:30 -0600

    arreglos

--- backend/routes/auditoria.js
+++ backend/routes/auditoria.js
@@ -4,7 +4,7 @@ const mysqlconexion = require('../db');
 
 
 router.get('/', (req, res) => {
-    mysqlconexion.query('SELECT ROW_NUMBER() OVER(ORDER BY pro.nombre) AS no, pro.nombre, pre.presentacion, pro.unidadMedida, l.fechaCad, l.correlativo as lote, k.correlativo as kardex, l.cantidad, l.precioUnitario, SUM(l.cantidad * l.precioUnitario) as total FROM lotes as l INNER JOIN producto as pro on l.idProducto = pro.id INNER JOIN presentacion as pre on l.idPresentacion = pre.id INNER JOIN detallekardex as de on l.id = de.idLote INNER JOIN kardex as k on k.id = de.idKardex;',
+    mysqlconexion.query('SELECT ROW_NUMBER() OVER(ORDER BY pro.nombre) AS no, pro.nombre, pre.presentacion, pro.unidadMedida, l.fechaCad, l.correlativo as lote, k.correlativo as kardex, l.cantidad, l.precioUnitario, (l.cantidad * l.precioUnitario) as total FROM lotes as l INNER JOIN producto as pro on l.idProducto = pro.id INNER JOIN presentacion as pre on l.idPresentacion = pre.id INNER JOIN detallekardex as de on l.id = de.idLote INNER JOIN kardex as k on k.id = de.idKardex;',
         (error, rows, fields) => {
             if (!error) {
                 res.json(rows);
