320b07408fb7a1f112f738c668f23208e233dd3a
Mario <daniellopezbosque@gmail.com>
2022-11-09 13:30:47 -0600

    logo

--- frontend/src/components/MenuBar/Navigate.js
+++ frontend/src/components/MenuBar/Navigate.js
@@ -6,6 +6,7 @@ import { SplitButton } from 'primereact/splitbutton';
 import { logout } from "../../actions/auth";
 import { useSelector } from "react-redux";
 import { clearMessage } from "../../actions/message";
+import logo from "../../images/fondo2.ico";
 const Navigation = () => {
 
     const dispatch = useDispatch();
@@ -298,7 +299,7 @@ const Navigation = () => {
         }
       }
     ]
-    const start = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="mr-2"></img>;
+    const start = <img alt="logo" src={logo} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="mr-2"></img>;
     const end = <SplitButton className="mr-2 mb-2 p-button-rounded p-button-info" label="Inicio" model={items} />
     const end2 = <SplitButton className="mr-2 mb-2 p-button-rounded p-button-success" label="Inicio"  model={items2}></SplitButton>
     return (
