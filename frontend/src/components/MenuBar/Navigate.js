ccd5eaeaaa5fe4bbc2cdc3974c36e87e97600830
Mario <daniellopezbosque@gmail.com>
2022-11-09 12:32:05 -0600

    arreglos navbar

--- frontend/src/components/MenuBar/Navigate.js
+++ frontend/src/components/MenuBar/Navigate.js
@@ -5,13 +5,7 @@ import { Menubar } from 'primereact/menubar';
 import { SplitButton } from 'primereact/splitbutton';
 import { logout } from "../../actions/auth";
 import { useSelector } from "react-redux";
-import navListAdmin from "./Items";
-import navListKardex from "./Items";
-import navListUsuario from "./Items";
-import navlistDespechador from "./Items";
-import navlistW from "./Items";
 import { clearMessage } from "../../actions/message";
-import logo from "../../images/fondo2.ico"
 const Navigation = () => {
 
     const dispatch = useDispatch();
@@ -304,10 +298,9 @@ const Navigation = () => {
         }
       }
     ]
-    const start = <img alt="logo" src={logo} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="mr-2"></img>;
+    const start = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="mr-2"></img>;
     const end = <SplitButton className="mr-2 mb-2 p-button-rounded p-button-info" label="Inicio" model={items} />
     const end2 = <SplitButton className="mr-2 mb-2 p-button-rounded p-button-success" label="Inicio"  model={items2}></SplitButton>
-    const end3 = <a onClick={logOut()} href="/login">Adios</a>
     return (
         <header>
       {currentUser && currentUser.Rol === "Administrador" ? (
