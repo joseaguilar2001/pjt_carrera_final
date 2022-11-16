aa8591068b654e851f7c5333048828a1ee370a74
Jose Aguilar <joseaguilar2001@hotmail.es>
2022-11-15 19:45:05 -0500

    .

--- frontend/src/components/Vistas/suministrosVistas.js
+++ frontend/src/components/Vistas/suministrosVistas.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { VistasService } from '../../services/VistasService';
 import Table from 'react-bootstrap/Table';
+import { useParams } from "react-router-dom
 const VistaTableSum = (props) => {
     const [suministros, setSuministros] = useState([]);
     const [kardex, setKardex] = useState([]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const vistaServicePedido = new VistasService();
-    const {idK} = props;
+    const {idK} = useParams();
     useEffect(() => {
         vistaServicePedido.readAllSuministros(idK)
         .then(data => setSuministros(data));
